985b690de5dd8105daefc41c91461c0546c52c12
Alan Cho <alancho397@gmail.com>
2023-07-12 01:09:46 -0500

    Added solution two

--- Object.js
+++ Object.js
@@ -1,21 +1,23 @@
 // 1)
+/*
 function createGreeter(name) {
   return {
     name: name,
     morning: "Good Morning",
     afternoon: "Good Afternoon",
     evening: "Good Evening",
+
     greet: function (timeOfDay) {
       let msg = "";
       switch (timeOfDay) {
         case "morning":
-          msg += `${morning} ${name}`;
+          msg += `${this.morning} ${this.name}`;
           break;
         case "afternoon":
-          msg += `${afternoon} ${name}`;
+          msg += `${this.afternoon} ${this.name}`;
           break;
         case "evening":
-          msg += `${evening} ${name}`;
+          msg += `${this.evening} ${this.name}`;
           break;
       }
 
@@ -26,3 +28,28 @@ function createGreeter(name) {
 
 let helloVictor = createGreeter("Victor");
 helloVictor.greet("morning"); //Good Morning Victor
+
+// 2)
+let item = {
+  name: "Foo",
+  description: "Fusce consequat dui est, semper.",
+  price: 50,
+  quantity: 100,
+
+  discount: function (percent) {
+    let discount = this.price * (percent / 100);
+    console.log((this.price -= discount));
+  },
+};
+
+let itemOne = Object.assign({}, item);
+itemOne.discount(20); // should return 40
+
+let itemTwo = Object.assign({}, item);
+itemTwo.discount(50); // should return 25
+
+let itemThree = Object.assign({}, item);
+itemThree.discount(25); // should return 37.5
+*/
+
+// 3)
