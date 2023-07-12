f4e2c0bfdb32a85f1044eeb5a81d017107ddc43e
Alan Cho <alancho397@gmail.com>
2023-07-12 00:42:09 -0500

    Added solution ten

--- Easy.js
+++ Easy.js
@@ -227,6 +227,85 @@ console.log(kitty.walk());
 let flash = new Cheetah("Flash");
 console.log(flash.walk());
 // "Flash runs forward"
-*/
 
 // 10)
+class Pet {
+  constructor(species, name) {
+    this.species = species;
+    this.name = name;
+  }
+}
+
+class Owner {
+  constructor(name) {
+    this.name = name;
+    this.pets = {};
+  }
+
+  numberOfPets() {
+    return Object.keys(this.pets).length;
+  }
+}
+
+class Shelter {
+  constructor() {
+    this.owners = [];
+  }
+
+  adopt(owner, pet) {
+    owner.pets[pet.name] = pet.species;
+
+    if (!this.owners.includes(owner)) {
+      this.owners.push(owner);
+    }
+  }
+
+  printAdoptions() {
+    console.log(this.owners);
+    for (let owner of this.owners) {
+      console.log(`${owner.name} has the following pets: `);
+      for (let pet in owner.pets) {
+        console.log(`A ${owner.pets[pet]} named ${pet}.`);
+      }
+    }
+  }
+}
+
+let butterscotch = new Pet("cat", "Butterscotch");
+let pudding = new Pet("cat", "Pudding");
+let darwin = new Pet("bearded dragon", "Darwin");
+let kennedy = new Pet("dog", "Kennedy");
+let sweetie = new Pet("parakeet", "Sweetie Pie");
+let molly = new Pet("dog", "Molly");
+let chester = new Pet("fish", "Chester");
+
+let phanson = new Owner("P Hanson");
+let bholmes = new Owner("B Holmes");
+
+let shelter = new Shelter();
+shelter.adopt(phanson, butterscotch);
+shelter.adopt(phanson, pudding);
+shelter.adopt(phanson, darwin);
+shelter.adopt(bholmes, kennedy);
+shelter.adopt(bholmes, sweetie);
+shelter.adopt(bholmes, molly);
+shelter.adopt(bholmes, chester);
+shelter.printAdoptions();
+console.log(`${phanson.name} has ${phanson.numberOfPets()} adopted pets.`);
+console.log(`${bholmes.name} has ${bholmes.numberOfPets()} adopted pets.`);
+
+
+P Hanson has adopted the following pets:
+a cat named Butterscotch
+a cat named Pudding
+a bearded dragon named Darwin
+
+B Holmes has adopted the following pets:
+a dog named Molly
+a parakeet named Sweetie Pie
+a dog named Kennedy
+a fish named Chester
+
+P Hanson has 3 adopted pets.
+B Holmes has 4 adopted pets.
+*/
