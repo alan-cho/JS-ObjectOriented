217cd1fbb655cdfa0d3c917a056bd38fcddf2e7b
Alan Cho <alancho397@gmail.com>
2023-07-11 23:38:01 -0500

    Initialize easy.js, added solution one and two

---
+++ Easy.js
@@ -0,0 +1,39 @@
+// 1)
+/*
+class Rectangle {
+  constructor(width, length) {
+    this.width = width;
+    this.length = length;
+  }
+
+  getWidth() {
+    return this.width;
+  }
+
+  getLength() {
+    return this.length;
+  }
+
+  getArea() {
+    return this.length * this.width;
+  }
+}
+
+let rect = new Rectangle(4, 5);
+
+console.log(rect.getWidth()); // 4
+console.log(rect.getLength()); // 5
+console.log(rect.getArea()); // 20
+
+// 2)
+class Square extends Rectangle {
+  constructor(side) {
+    super(side, side);
+  }
+}
+
+let square = new Square(5);
+console.log(`area of square = ${square.getArea()}`); // area of square = 25
+*/
+
+// 3)
