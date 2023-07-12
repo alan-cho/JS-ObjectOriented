65cd3c73915f5d0d3776c7a0b8026857f3bf549c
Alan Cho <alancho397@gmail.com>
2023-07-12 03:51:19 -0500

    Added solution two

--- Classes.js
+++ Classes.js
@@ -3,6 +3,11 @@
 console.log("Hello".constructor.name);
 console.log([1, 2, 3].constructor.name);
 console.log({ name: "Srdjan" }.constructor.name);
-*/
 
 // 2)
+class Cat {
+  constructor() {}
+}
+*/
+
+// 3)
