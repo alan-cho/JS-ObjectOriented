8b109d12b5daafa057c8fbd5621dce098552ea81
Alan Cho <alancho397@gmail.com>
2023-07-12 05:07:57 -0500

    Added solution three

--- ObjectCreationPatterns.js
+++ ObjectCreationPatterns.js
@@ -135,3 +135,75 @@ graduateStudent.research(); // logs 'Researching'
 */
 
 // 3)
+class CircularQueue {
+  constructor(size) {
+    this.buffer = new Array(size).fill(null);
+    this.nextPosition = 0;
+    this.oldestPosition = 0;
+  }
+
+  enqueue(object) {
+    if (this.buffer[this.nextPosition] !== null) {
+      this.oldestPosition = this.increment(this.nextPosition);
+    }
+    this.buffer[this.nextPosition] = object;
+    this.nextPosition = this.increment(this.nextPosition);
+  }
+
+  dequeue() {
+    let value = this.buffer[this.oldestPosition];
+    this.buffer[this.oldestPosition] = null;
+    if (value !== null) {
+      this.oldestPosition = this.increment(this.oldestPosition);
+    }
+    return value;
+  }
+
+  increment(position) {
+    return (position + 1) % this.buffer.length;
+  }
+}
+
+let queue = new CircularQueue(3);
+console.log(queue.dequeue() === null);
+
+queue.enqueue(1);
+queue.enqueue(2);
+//[1, 2]
+console.log(queue.dequeue() === 1);
+//[2]
+queue.enqueue(3);
+queue.enqueue(4);
+//[2, 3, 4]
+console.log(queue.dequeue() === 2);
+//[3, 4]
+queue.enqueue(5);
+//[3, 4, 5]
+queue.enqueue(6);
+//[6, 4, 5]
+queue.enqueue(7);
+//[6, 7, 5]
+console.log(queue.dequeue() === 5);
+console.log(queue.dequeue() === 6);
+console.log(queue.dequeue() === 7);
+console.log(queue.dequeue() === null);
+
+let anotherQueue = new CircularQueue(4);
+console.log(anotherQueue.dequeue() === null);
+
+anotherQueue.enqueue(1);
+anotherQueue.enqueue(2);
+console.log(anotherQueue.dequeue() === 1);
+
+anotherQueue.enqueue(3);
+anotherQueue.enqueue(4);
+console.log(anotherQueue.dequeue() === 2);
+
+anotherQueue.enqueue(5);
+anotherQueue.enqueue(6);
+anotherQueue.enqueue(7);
+console.log(anotherQueue.dequeue() === 4);
+console.log(anotherQueue.dequeue() === 5);
+console.log(anotherQueue.dequeue() === 6);
+console.log(anotherQueue.dequeue() === 7);
+console.log(anotherQueue.dequeue() === null);
