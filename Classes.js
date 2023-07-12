c4e02f0d2e195397b24c4477d8eec893c3d16fdb
Alan Cho <alancho397@gmail.com>
2023-07-12 03:56:59 -0500

    Added solution seven

--- Classes.js
+++ Classes.js
@@ -17,6 +17,17 @@ class Cat {
 
 let kitty = new Cat("Sophie");
 kitty.greet();
-*/
 
 // 7)
+class Person {
+  constructor(name = "John Doe") {
+    this.name = name;
+  }
+}
+
+let person1 = new Person();
+let person2 = new Person("Pepe");
+
+console.log(person1.name); // John Doe
+console.log(person2.name); // Pepe
+*/
